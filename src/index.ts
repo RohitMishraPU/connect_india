6db0de8600d4a0d171f10963b393be8b2565d852
RohitMishraPU <rohitrklpny@gmail.com>
2023-03-29 00:01:00 +0530

    Added login logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import authRouter from'./routes/authRoutes'
+import authRouter from'./routes/authRoutes';
+import session from 'express-session';
+import passport from 'passport';
+import flash from 'express-flash';
+import cors from 'cors';
+
+const allowedOrigins = ['http://localhost:3001']; // add your allowed origin here
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 
 if(process.env.TS_NODE_DEV)
 	require('dotenv').config();
@@ -8,10 +25,17 @@ if(process.env.TS_NODE_DEV)
 const app = express();
 
 //const authRouter = require('./src/routes/authRoutes');
+app.use(cors(corsOptions));
 
 app.use(express.json());
-
-app.use('/auth', authRouter);
+app.use(flash());
+app.use(session({
+	secret : process.env.SESSION_SECRET,
+	resave : false,
+	saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 try{
 	 if(process.env.DATABASE_URL)
@@ -22,16 +46,31 @@ try{
 	db.once('open', ()=> console.log('Connected wiwth DB'));
 	db.on('error', error =>  console.log(error));
 
+	app.use('/auth',checkNotAuthenticated, authRouter);
+
 	// Generic routes
-	app.get('/', (req,res)=>{
+	app.get('/', checkAuthenticated, (req,res)=>{
 		res.send('HEY THERE!! Holaaa');
 	});
 
-	app.post('/', (req,res)=>{
-		console.log(req?.body || 'No Body');
-		res.send(' Post HEY THERE!! Holaaa');
-	});
 	app.listen(3000, ()=> console.log('App is listening @3000'));
+
+	function checkAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) {
+			console.log(req);
+		  return next()
+		}
+	  
+		res.status(401).json({message : 'Unauthorized'});
+	  }
+
+	  function checkNotAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) {
+			return res.status(400).json({message : 'User is already logged in'});
+		}
+	  	return next()
+	  }
+
 }catch(error){
 	console.log(error);
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,36 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Users from '../models/Users';
 import bcrypt from 'bcrypt';
+import passport from 'passport';
+import createPassportLocalStrategy from '../passportConfig';
+
 
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-	res.send('Hollaaa from auth');
-});
 
-router.post('/signup', async(req,res)=>{
-	if(!(req.body.username && req.body.password)) res.status(400).json({ message : 'Username and Password are mandatory'});
+export const fetchUserByEmail = async (email : string) =>{
+	try{
+		return await Users.findOne({email : email});
+	}catch(err){
+		throw err.message;
+	}
+};
+
+export const fetchUserByID = async (id : string) =>{
+	try{
+		return await Users.findById(id);
+	}catch(err){
+		throw err.message;
+	}
+};
+
+
+createPassportLocalStrategy(passport, fetchUserByEmail, fetchUserByID);
+
+
+router.post('/signup', async(req : Request,res : Response)=>{
+	if(!(req.body.email && req.body.password)) res.status(400).json({ message : 'Email and Password are mandatory'});
 	const hashedPassword = await bcrypt.hash(req.body.password, 10); 
 
 	const user = new Users({
@@ -25,5 +46,34 @@ router.post('/signup', async(req,res)=>{
 		res.status(400).json({message : err?.message || 'Something went wrong'});
 	}
 });
+
+// router.post('/login', passport.authenticate('local'), (req : AuthenticatedRequest, res) => {
+// 	res.status(200).json(req.user);
+// });
+
+router.post('/login', (req : Request, res : Response, next : NextFunction)=>{
+	const { email, password } = req.body;
+
+  	if (!email || !password) 
+    	return res.status(400).json({ error: 'Email and password are required' });
+
+	return next();
+},(req : Request, res : Response, next : NextFunction)=> {
+	passport.authenticate('local', (err, user, info) => {
+	  if (err) {
+		return next(err);
+	  }
+	  if (!user) {
+
+		return res.status(401).json({ error: info.message });
+	  }
+	  (req as any).logIn(user, (err) => {
+		if (err) {
+		  return next(err);
+		}
+		return res.status(200).json(user);
+	  });
+	})(req, res, next);
+  });
 
 export default router;
